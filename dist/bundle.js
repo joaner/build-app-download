e09978ddcbb9a55c3e29630649f56680fdc38652
xiaokang pu <pxk@seiue.com>
2018-02-21 13:33:50 +0800

    build

--- dist/bundle.js
+++ dist/bundle.js
@@ -67,20 +67,6 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(1);
-module.exports = __webpack_require__(2);
-
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports, __webpack_require__) {
-
 module.exports = __webpack_require__.p + "index.html";
 
 /***/ })
